6949be52d16735b46dfc8f70ecc18b10563b38e9
Kiril <kiril@Mac-jenya.local>
2025-08-20 18:04:22 +0300

    advantage-section

---
+++ src/components/TechnologiesMain/TechnologiesMain.jsx
@@ -0,0 +1,23 @@
+import './style.css'
+
+function TechnologiesMain(){
+    return(
+        <div className="w-full h-48 overflow-x-hidden whitespace-nowrap flex justify-center items-center max-w-[500px] mx-auto mt-20">
+            <div className="inline-flex gap-20 px-4 slider">
+                <img src="/image/tailwind.svg" alt="Image 1" className="h-10 w-auto flex-shrink-0" />
+                <img src="/image/react.svg" alt="Image 2" className="h-10 w-auto flex-shrink-0" />
+                <img src="/image/django.svg" alt="Image 1" className="h-10 w-auto flex-shrink-0" />
+                <img src="/image/python.svg" alt="Image 2" className="h-10 w-auto flex-shrink-0" />
+                <img src="/image/vite.svg" alt="Image 1" className="h-10 w-auto flex-shrink-0" />
+                <img src="/image/nodejs.svg" alt="Image 1" className="h-10 w-auto flex-shrink-0" />
+                <img src="/image/tailwind.svg" alt="Image 1" className="h-10 w-auto flex-shrink-0" />
+                <img src="/image/react.svg" alt="Image 2" className="h-10 w-auto flex-shrink-0" />
+                <img src="/image/django.svg" alt="Image 1" className="h-10 w-auto flex-shrink-0" />
+                <img src="/image/python.svg" alt="Image 2" className="h-10 w-auto flex-shrink-0" />
+                <img src="/image/vite.svg" alt="Image 1" className="h-10 w-auto flex-shrink-0" />
+                <img src="/image/nodejs.svg" alt="Image 1" className="h-10 w-auto flex-shrink-0" />
+            </div>
+        </div>
+    );
+}
+export default TechnologiesMain;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ListView from "../../components/ProjectList/ProjectList";
 import Navbar from '../../components/NavBar/Navbar';
 import MainSection from '../../components/MainSection/MainSection';
+import TechnologiesMain from '../../components/TechnologiesMain/TechnologiesMain';
 
 function HomePage(){
     return (
         <>
         <Navbar />
         <MainSection />
+        <TechnologiesMain />
         </>
     );
 } 
